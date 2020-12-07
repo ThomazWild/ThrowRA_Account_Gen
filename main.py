e6393b3c90e252146acc19bb8dfaa17c40c23f1d
Thomaz <50690864+ThomazWild@users.noreply.github.com>
2020-12-07 15:26:07 +1100

    new

---
+++ main.py
@@ -0,0 +1,55 @@
+import random
+import pandas as pd
+first_names = pd.read_csv("DATA/names.csv")
+
+data = first_names['John']
+
+data = list(data)
+
+n = random.randint(0,100)
+extrax = ['.','_','-','the','and']
+
+if n <= 10:
+    P_1 = random.choice(data) + random.choice(extrax) + random.choice(data)
+elif n in range(10,36):
+    P_1 = random.choice(data) + str(random.randint(1950,2020))
+elif n in range(36,50):
+    P_1 = random.choice(data) + random.choice(data)
+elif n in range(50,75):
+    P_1 = random.choice(data) + random.choice(data) + str(random.randint(1950,2020))
+elif n in range(75,100):
+    P_1 = random.choice(data) + random.choice(data) + str(random.randint(1,30)) + str(random.randint(1,12))
+else:
+    P_1 = "Good luck next time"
+
+domains = [ "hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
+
+n = random.randint(0,100)
+
+if n <= 50:
+    P_2 = random.choice(domains)
+else:
+    P_2 = random.choice(data) + ".com"
+
+domain_country = [".eu", ".ru", ".bru",
+                  '.cn','.de','.tk',
+                  '.uk','.tw','.fr',
+                  '.br','.vn','.au','.us']
+
+m = random.randint(0,100)
+
+if m <= 50:
+    account = P_1 + "@" + P_2
+else:
+    account = P_1 + "@" + P_2 + random.choice(domain_country)
+
+print(account)
+
+#Password generator
+
+pass_list = pd.read_csv("DATA/words.txt",header=None)
+pass_list = list(pass_list[0])
+
+password = random.choice(pass_list) + str(random.randint(10000, 9999999))
+
+print(password)
